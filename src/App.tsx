672066406bcf7fb3c23a2f22db017f596f57c0f7
Simão Barbosa <simao.barbosa.05@gmail.com>
2025-01-12 14:55:54 +0000

    added repeating background

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import "./App.css";
+import brickBackground from "./assets/brick.png";
 
 function App() {
   return (
     <>
-      <h1 className="text-purple-300">find your closest stronghold</h1>
+      <div
+        className="Component-Bg"
+        style={{
+          backgroundImage: `url(${brickBackground})`,
+          backgroundSize: "75px",
+          backgroundPosition: "center center",
+          backgroundRepeat: "repeat",
+          width: "100vw",
+          height: "100vh",
+        }}
+      >
+        <h1 className="text-black font-extrabold text-center pt-10">find your closest stronghold</h1>
+      </div>
     </>
   );
 }
